b9ffacdaff7d48e04286a508088cb1ac680a377e
ednese <evancours94@gmail.com>
2020-11-05 01:22:19 +0100

    v1 of 1st page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,7 +4,8 @@ import './contact.scss'
 function Contact() {
   return (
     <div className="contact">
-        <div className="contact__inner">
+        <div className="contact__inner" data-sal-duration="500" data-sal="slide-up"
+                data-sal-delay="200" data-sal-easing="ease-out-bounce">
             contact me:
             <a className="contact_red" href="tel:+33651102150"> 06.51.10.21.50 </a>
             or
